cdbcc383c11d01ad6591cbec7323eaa2ce8c08ac
Ryan McDonough <ryan.mcdonough@epiphanysolutions.co.uk>
2017-05-17 16:58:24 +0100

    Just after initial commit.

---
+++ TweetSite/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using TweetSite.Core;
+using static System.String;
+
+namespace TweetSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var numbers = new List<long>();
+
+            if (HttpContext.Request.Query["tweets"].Count != 0)
+            {
+                var tweets = HttpContext.Request.Query["tweets"].ToString();
+
+                numbers = tweets.Split(',').Select(long.Parse).ToList();
+
+            }
+
+            var generate = new Generate();
+
+            return Content(System.Net.WebUtility.HtmlDecode(generate.SiteFromTweets(numbers)),"text/html");
+        }
+    }
+}
---
+++ TweetSite/Core/Generate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tweetinvi;
+
+namespace TweetSite.Core
+{
+    public class Generate
+    {
+        public string SiteFromTweets(List<long> tweets)
+        {
+            //Create any app at: https://apps.twitter.com/
+            //Generate the Consumer Secret and Key, then generate your user token and secret as well.
+            Auth.SetUserCredentials("ConsumerKey", "ConsumerSecret", "UserAccessToken", "UserAccessSecret");
+            return tweets.Count == 0 ? "<p class=MsoHeader><span lang=EN-US style='mso-ansi-language:EN-US'>Here's some bad HTML - add some ?tweets=864849490162876416,864849511268716545,864849695797129218,864849714252001280 maybe<o:p></o:p></span></p>" : tweets.Select(Tweet.GetTweet).Aggregate("", (current, tweetToReplyTo) => current + tweetToReplyTo.Text);
+        }
+    }
+}
